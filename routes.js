3875f4e8318e64d1bff629243679e479e2307451
Lucas Mendonça <lucas_lam@live.com>
2019-06-12 10:05:25 -0300

    resolved

--- routes.js
+++ routes.js
@@ -1,23 +1,38 @@
-const express = require('express');
-const XLSX = require('xlsx');
-const fs = require('fs');
+const express = require("express");
+const XLSX = require("xlsx");
+const fs = require("fs");
 
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
+routes.get("/", (req, res) => {
   var buf = fs.readFileSync("teste.xlsx");
-  var wb = XLSX.read(buf, {raw:true});
-  
+  var wb = XLSX.read(buf, { raw: true });
+
   var first_sheet_name = wb.SheetNames[0];
 
   var worksheet = wb.Sheets[first_sheet_name];
 
+  let arr = [];
 
   for (const item in worksheet) {
-    console.log(worksheet[item].v);
+    if (!/^!/.test(item)) arr.push(worksheet[item].v);
   }
 
-  res.send(worksheet);
+  let header = arr.slice(0, 3);
+  let content = arr.slice(3);
+  let locals = [];
+  let inc = [];
+
+  for (let i = 0; i < content.length; i++) {
+    inc.push(content[i]);
+    if ((i + 1) % 3 === 0) {
+      locals.push(inc);
+      inc = [];
+    }
+  }
+  const result = { header, locals };
+
+  res.send(result);
 });
 
 module.exports = routes;
