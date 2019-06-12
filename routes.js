2fa7e236954bdbd62e83d02cf6f78cf806010e69
Victor Pragana <victormerigue@gmail.com>
2019-06-12 17:57:48 -0300

    file can now be sent by post method

--- routes.js
+++ routes.js
@@ -1,16 +1,22 @@
 const express = require("express");
+const path = require("path");
 const fs = require("fs");
 const xlsx = require("node-xlsx").default;
 const User = require("./model");
+const multer = require("multer");
+const uploadConfig = require("./upload");
 
 const routes = express.Router();
 
+const upload = multer(uploadConfig);
+
 routes.get("/", async (req, res) => {
   // Parse a buffer
-  const workSheetsFromBuffer = xlsx.parse(fs.readFileSync(`teste.xlsx`));
+  const sheets = xlsx.parse(fs.readFileSync(`teste.xlsx`));
+  const data = sheets[0].data;
 
-  for (let item = 1; item < workSheetsFromBuffer[0].data.length; item++) {
-    const [nome, idade, sexo] = workSheetsFromBuffer[0].data[item];
+  for (let item = 1; item < data.length; item++) {
+    const [nome, idade, sexo] = data[item];
     const obj = {
       nome,
       idade,
@@ -19,7 +25,27 @@ routes.get("/", async (req, res) => {
     await User.create(obj);
   }
 
-  res.send(workSheetsFromBuffer[0].data);
+  res.send(data);
+});
+
+routes.post("/", upload.single("excel"), async (req, res) => {
+  const sheets = xlsx.parse(
+    fs.readFileSync(
+      path.resolve(__dirname, "files", `${req.file.originalname}`)
+    )
+  );
+  const data = sheets[0].data;
+
+  for (let item = 1; item < data.length; item++) {
+    const [nome, idade, sexo] = data[item];
+    const obj = {
+      nome,
+      idade,
+      sexo
+    };
+    await User.create(obj);
+  }
+  res.json({ ok: true });
 });
 
 module.exports = routes;
